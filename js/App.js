7b26ca05c633e9f62d8bd5c6499af0fc7aaef8d0
scheifen <scheifen@gmail.com>
2014-06-20 13:56:19 -0500

    up to detail routing. errors with constructed route for detailCtrl

--- js/App.js
+++ js/App.js
@@ -10,10 +10,10 @@ messageApp.config(['$routeProvider',
 	 	when('/', {
 	 		templateUrl: 'partials/message-index.html',
 	 	}).
-	 	// when('/:messageId', {
-	 	// 	templateUrl: 'partials/message-detail.html',
-	 	// 	controller:'detailCtrl',		
-	 	// }).
+	 	when('/messages/:message_id', {
+	 		templateUrl: 'partials/message-detail.html',
+	 		controller:'detailController',		
+	 	}).
 	 	otherwise({
 	 		redirectTo: '/'
 	 	});
@@ -22,5 +22,13 @@ messageApp.config(['$routeProvider',
 messageApp.controller('indexController', ['$http', function($http){
 	var index = this;
 	this.messages = {};
-	$http.get('/locust-sms-prototype/FakeMessages.json').success(function(data){index.messages=data});
+	$http.get('/locust-sms-prototype/FakeMessages.json').success(function(data){
+		index.messages=data});
+}]);
+
+messageApp.controller('detailController', ['$http', '$routeParams', function($http, $routeParams){
+	var detail = this;
+	$http.get('/locusts-sms-prototype/messages/'+$routeParams.message_id+'.json').success(function(data){
+		detail.message = data;
+	});
 }]);
